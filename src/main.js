d2d2e473a5ae708bf9d927a446f1ed33929d5376
周忠玉 <10162316@qq.com>
2026-03-05 16:07:22 +0800

    feat: 更新网页图标，修改远程字体

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@
  */
 
 import { createApp } from 'vue'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/jetbrains-mono/500.css'
 import './style.css'
 import App from './App.vue'
 import router from './router'
